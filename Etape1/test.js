a5f886208d98ed77f491d5cd06686f515692df26
Delor_r <you@example.com>
2016-02-02 19:38:54 +0100

    modif pour que les secondes soit des heures

--- Etape1/test.js
+++ Etape1/test.js
@@ -4,6 +4,6 @@ var ncp = require("ncp");
 var fs = require('fs-extra');
 var myArgs = process.argv.slice(2);
 var myArgs = process.argv.slice(2)[0];
-var time = process.argv.slice(2)[1]* 1000;
+var time = process.argv.slice(2)[1] * 60 * 60 * 1000;
 
 setInterval(function(){fs.copy(myArgs, '/Users/delor_r/Desktop/Backup')},time);
