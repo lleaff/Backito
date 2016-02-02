93ec33a94aa4a71c1c9a4b45f0debdd63409c32c
lleaff <lleaff@users.noreply.github.com>
2016-02-02 19:57:16 +0100

    test

--- Etape1/test.js
+++ Etape1/test.js
@@ -6,4 +6,6 @@ var myArgs = process.argv.slice(2);
 var myArgs = process.argv.slice(2)[0];
 var time = process.argv.slice(2)[1] * 60 * 60 * 1000;
 
-setInterval(function(){fs.copy(myArgs, '/Users/delor_r/Desktop/Backup')},time);
+setInterval(function(){
+    fs.copy(myArgs, '/Users/delor_r/Desktop/Backup')
+}, time);
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 
-var argv = require('yargs').argv;
+var argv = require('yargs')
+    .boolean(['r'])
+    .alias('r', 'recursive')
+    .alias('d', 'dest')
+    .argv;
 
 console.log("process.argv", process.argv);
 console.log("yargs.argv", argv);
-
-console.log("-m", argv.m);
