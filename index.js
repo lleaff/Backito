56071e9e3613599581c4a31705c6cba4112106d1
amira_s <asdfsdf@sdf.com>
2016-02-03 10:20:56 +0100

    wip file testing

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 #!/usr/bin/env node --harmony-destructuring
 
+const fs = require('fs');
 var { compose, curry } = require('ramda');
+var utils = require('./src/utils/utils.js');
 
 var argv = require('yargs')
+    .usage('Usage: $0 [-r | -recursive] path [...] [-d | -dest destination] [-s | -schedule crontab_string]')
     .boolean(['r'])
     .alias('r', 'recursive')
     .alias('d', 'dest')
+    .alias('s', 'schedule')
     .argv;
 
-console.log("process.argv", process.argv);
-console.log("yargs.argv", argv);
+console.log("yargs.argv", argv, "\n");
+utils.map(argv._, function(path){
+	fs.stat(path, function(err, stat) {
+	    if(err == null) {
+	        console.log('File exists');
+	    	if (stat.isDirectory(path))
+				console.log(path, "is a directory.");
+			else if (stat.isFile(path))
+				console.log(path, "is a file.");
+			return stat;
+	    }
+	    else
+	    	return console.log(path, "doesn't exist.");
+	});	
+}, function(stats){
+	console.log(stats);
+});
