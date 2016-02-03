73fe3e855c298fca561b166ce3cf7e30094c17a0
lleaff <lleaff@users.noreply.github.com>
2016-02-03 10:18:43 +0100

    fix

--- src/utils/asyncForEach.js
+++ src/utils/asyncForEach.js
@@ -2,7 +2,7 @@ module.exports = function asyncForEach(array, fn, callback) {
     var processedIndexes = [];
     array.map(function (val, i, arr) {
         fn(val, _ => processedIndexes[i] = true);
-        if (array.all((_, i) => processedIndexes[i]))
+        if (array.every((_, i) => processedIndexes[i]))
             callback(newArr);
     });
 }
--- src/utils/asyncMap.js
+++ src/utils/asyncMap.js
@@ -3,7 +3,7 @@ module.exports = function asyncMap(array, fn, callback) {
     var processedIndexes = [];
     array.map(function (val, i, arr) {
         newArr[i] = fn(val, _ => processedIndexes[i] = true);
-        if (array.all((_, i) => processedIndexes[i]))
+        if (array.every((_, i) => processedIndexes[i]))
             callback(newArr);
     });
 }
