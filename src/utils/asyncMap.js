ce69b98161fae9808e7d63a5062faa0ad1122a71
lleaff <lleaff@users.noreply.github.com>
2016-02-03 10:17:45 +0100

    fix

--- src/utils/asyncMap.js
+++ src/utils/asyncMap.js
@@ -1,4 +1,4 @@
-module.exports.asyncMap = function asyncMap(array, fn, callback) {
+module.exports = function asyncMap(array, fn, callback) {
     var newArr = [];
     var processedIndexes = [];
     array.map(function (val, i, arr) {
